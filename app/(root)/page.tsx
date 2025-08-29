494e8c84dc114e50ef76d7973ddaf9c51012bb46
Abdalla-Abukhris <abdulla.abukres@gmail.com>
2025-08-29 23:48:30 +0200

    auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,16 +4,26 @@ import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
 import { sampleBooks } from "@/constants";
 
-const Home = () => (
-  <>
-    <BookOverview {...sampleBooks[0]} />
+const Home = async () => {
+  const session = await auth();
 
-    <BookList
-    title="Latest Books"
-    books=[sampleBooks]
-    containerClassName="mt-28"
-    />
-  </>
-);
+  /*const latestBooks = (await db
+    .select()
+    .from(books)
+    .limit(10)
+    .orderBy(desc(books.createdAt))) as Book[];*/
+
+  return (
+    <>
+      <BookOverview {...latestBooks[0]} userId={session?.user?.id as string} />
+
+      <BookList
+        title="Latest Books"
+        books={latestBooks.slice(1)}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
 
 export default Home;
